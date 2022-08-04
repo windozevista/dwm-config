c39f0fb73a1bc5c389289a760ecfbcf4f7b861d7
windozevista <109780222+windozevista@users.noreply.github.com>
2022-08-03 22:22:06 -0400

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -9,21 +9,15 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-/*static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#478061"; */
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#504945";
-static const char col_gray3[]       = "#bdae93";
-static const char col_gray4[]       = "#ebdbb2";
-static const char col_cyan[]        = "#cc241d";
+static const char col_cyan[]        = "#B4BEFE"; 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	/*[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },*/
-	[SchemeSel]  = { col_gray4, col_gray2,  col_gray1  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
